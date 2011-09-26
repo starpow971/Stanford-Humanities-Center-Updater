ec07d3f15c016730a77d48a9c5d49ea05dab2201
Christine Williams <christine.bennett.williams@gmail.com>
2011-09-25 17:48:01 -0700

    boilerplate for gcal.py

---
+++ gcal.py
@@ -0,0 +1,6 @@
+# Copyright 2011, The Board of Regents of Leland Stanford, Jr. University
+# All rights reserved. See LICENSE. 
+# Author: Christine Williams <christine.bennett.williams@gmail.com>
+# Description: interface with calendar server, parse calendar responses, 
+# produce useful event objects.
+
