ae683afb16e98ae0b0c15b3930139df1b3561d34
Christine Williams <christine.bennett.williams@gmail.com>
2011-09-25 17:57:54 -0700

    make gcal_test.py runnable

--- gcal_test.py
+++ gcal_test.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
 # Copyright 2011, The Board of Regents of Leland Stanford, Jr. University
 # All rights reserved. See LICENSE. 
 # Author: Christine Williams <christine.bennett.williams@gmail.com>
 # Description: interface with calendar server, parse calendar responses, 
 # produce useful event objects.
+
+import unittest
+
+
+class GcalTest(unittest.TestCase):
+	pass
+	
