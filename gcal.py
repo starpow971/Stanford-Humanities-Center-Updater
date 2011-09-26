33b40bc3c9d9443f10d953a647aa1ee386c7ae52
Christine Williams <christine.bennett.williams@gmail.com>
2011-09-26 08:26:57 -0700

    Not working

--- gcal.py
+++ gcal.py
@@ -4,3 +4,24 @@
 # Description: interface with calendar server, parse calendar responses, 
 # produce useful event objects.
 
+import json
+
+class Event:
+	"""Represents a Google Calendar event."""
+	
+	def __init__(self, calendar_title="", event_title="", event_id="",
+	             start_time=None, end_time=None, location="", status="", 
+	             description=""):
+		self.calendar_title = calendar_title
+		self.event_title = event_title
+		self.event_id = event_id
+		self.start_time = start_time  # A datetime.
+		self.end_time = end_time  # A datetime.
+		self.location = location
+		self.status = status  # like "confirmed" or something.
+		self.description = description
+
+
+def parseFeed(data):
+	parsed = json.loads(data)
+	print parsed
