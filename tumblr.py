fb8fbf567d37287cb68bf5c71821afef745016c9
Christine Williams <christine.bennett.williams@gmail.com>
2011-10-03 13:24:08 -0700

    Added encoding to heading; didn't work.

--- tumblr.py
+++ tumblr.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Copyright 2011, The Board of Regents of Leland Stanford, Jr. University
 # All rights reserved. See LICENSE. 
 # Author: Christine Williams <christine.bennett.williams@gmail.com>
