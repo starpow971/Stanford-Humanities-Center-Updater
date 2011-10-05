0deee1b7372e3fcaeb0983490b0c819908656055
Christine Williams <christine.bennett.williams@gmail.com>
2011-10-05 16:30:53 -0700

    Adds tumblr library

--- schema.sql
+++ schema.sql
@@ -1,2 +1,5 @@
 create table events(id, calendar_title TEXT, event_title TEXT, start_time INTEGER, end_time INTEGER,
                     location TEXT, status TEXT, description TEXT);
+                    
+                    
+create table news(post_title TEXT, post_date INTEGER, post_content TEXT, post_category TEXT);
