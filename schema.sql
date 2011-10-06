915db52ee91d57dd7a023570bd43fb398e60e189
Christine Williams <christine.bennett.williams@gmail.com>
2011-10-06 14:50:57 -0700

    Drop tables in schema.sql to avoid extra step of removing db file.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
+drop table if exists events;
+drop table if exists news;
+
+
 create table events(id, calendar_title TEXT, event_title TEXT, start_time INTEGER, end_time INTEGER,
                     location TEXT, status TEXT, description TEXT);
                     
