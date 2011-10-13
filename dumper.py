9c09c1ecce2b6e8e6ae9400aeb1cfe7defb0793c
Christine Williams <christine.bennett.williams@gmail.com>
2011-10-13 16:53:43 -0700

    Adds updated-min to calendar query to get all initial events in database.

--- dumper.py
+++ dumper.py
@@ -10,7 +10,7 @@ import datastore
 import gcal
 import tumblr
 
-events = gcal.parse_feed(urllib2.urlopen("https://www.google.com/calendar/feeds/stanford.humanities.events%40gmail.com/public/basic?showdeleted=true"))
+events = gcal.parse_feed(urllib2.urlopen("https://www.google.com/calendar/feeds/stanford.humanities.events%40gmail.com/public/basic?showdeleted=true&updated-min=2011-09-26T01:00:00-08:00&max-results=1000"))
 news = tumblr.parse_rss(urllib2.urlopen("http://stanfordhumanitiescenter.tumblr.com/rss"))
 
 store = datastore.load("database.db")
