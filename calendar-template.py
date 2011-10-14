2329b0830353ccf246dcc4113a2917cbd759c72c
Christine Williams <christine.bennett.williams@gmail.com>
2011-10-14 15:34:56 -0700

    Makes local template definition; works.

--- calendar-template.py
+++ calendar-template.py
@@ -7,7 +7,17 @@
 
 from Cheetah.Template import Template
 
-t = Template(file="calendar-landing-page.tmpl")
+templateDef = """
+	<html>
+	<head><title>Calendar</title></head>
+	<body>
+	<h1>Events Calendar</h1><br /><br /><h2>Upcoming Events</h2><br /><br />
+	<span style="font-size:11px; ">$calendar_title</span><span style="font-size:13px; "><br />
+	</span><span style="font-size:14px; color:#70694E;font-weight:bold; ">$event_title</span>
+	<span style="font-size:13px; "><br />$date | $start_time-$end_time | $location<br /><br /><hr>
+	<br /></span>
+	</body>
+	</html>"""
 
 class CalendarTemplate(Template):
 	calendar_title = 'STANFORD HUMANITIES CENTER EVENTS'
@@ -17,6 +27,6 @@ class CalendarTemplate(Template):
 	end_time = '6 pm'
 	location = 'Humanities Center Board Room'
 	
-t2 = CalendarTemplate(Template)
+t2 = CalendarTemplate(templateDef)
 
 print t2
