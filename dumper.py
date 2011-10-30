0734be64c7a6c6994bf167cfeb8fc323b14ef3c1
Scott Williams <scottw@artesiancode.com>
2011-10-30 15:01:50 -0700

    Thread urllib2 fetches, bring dumper runtime from 30 seconds to 2. Yay

--- dumper.py
+++ dumper.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 # Copyright 2011, The Board of Regents of Leland Stanford, Jr. University
 # All rights reserved. See LICENSE. 
 # Author: Christine Williams <christine.bennett.williams@gmail.com>
 # Description: Dump events from calendar and tumblr feeds straight into database.
 
-
+import cStringIO
+import threading
 import urllib2
 
 import datastore
@@ -30,15 +32,37 @@ calendar_ids = {
   "poetics": "ht02jaieajfqdkqogpcsso40cs@group.calendar.google.com"
 }
 
-events = []
-for calendar_id in calendar_ids.itervalues():
-  url = ("https://www.google.com/calendar/feeds/" + calendar_id + "/public/basic?"
-         "showdeleted=true&updated-min=2011-08-01T01:00:00-08:00&max-results=1000")
-  events += gcal.parse_feed(urllib2.urlopen(url))
+class FetchThread(threading.Thread):
+  """A worker thread that fetches a url."""
+  def __init__(self, url):
+    threading.Thread.__init__(self, name="Url fetcher for url %s" % url)
+    self.url = url
+    self.document = None
 
-news = tumblr.parse_rss(urllib2.urlopen("http://stanfordhumanitiescenter.tumblr.com/rss"))
+  def run(self):
+    # Force the url library to read the whole document.
+    self.document = cStringIO.StringIO(urllib2.urlopen(self.url).read())
 
-store = datastore.load("database.db")
-store.update(events, news)
-store.save()
-store.close()
+
+def main():
+  gcal_template = (
+      "http://www.google.com/calendar/feeds/%s/public/basic?"
+      "showdeleted=true&updated-min=2011-08-01T01:00:00-08:00&max-results=1000")
+  threads = [FetchThread(gcal_template % cal_id)
+             for cal_id in calendar_ids.itervalues()]
+  map(FetchThread.start, threads)  # Do work.
+  map(FetchThread.join, threads)  # Wait for work to end.
+  events = []
+  for t in threads:
+    events.extend(gcal.parse_feed(t.document))
+
+  news = tumblr.parse_rss(
+      urllib2.urlopen("http://stanfordhumanitiescenter.tumblr.com/rss"))
+
+  store = datastore.load("database.db")
+  store.update(events, news)
+  store.save()
+  store.close()
+
+if __name__ == '__main__':
+  main()
